b979669a0c748fbc2ba4570c3d1ac6b2faa5a5e0
teenaa <teenaafox@gmail.com>
2024-09-05 10:33:06 +0900

    이전 방식

---
+++ test2.py
@@ -0,0 +1,43 @@
+i = 1
+while i <= 5:
+    j = 1
+    while j <= i:
+        print("*", end="")
+        j += 1
+    print()
+    i += 1
+
+i = 1
+while i <= 5:
+    j = 1
+    while j <= 5 - i:
+        print(" ", end="")
+        j += 1
+    j = 1
+    while j <= i:
+        print("*", end="")
+        j += 1
+    print()
+    i += 1
+
+i = 1
+while i <= 5:
+    j = 1
+    if i <= 3:
+        while j <= 3 - i:
+            print(" ", end="")
+            j += 1
+        j = 1
+        while j <= 2 * i - 1:
+            print("*", end="")
+            j += 1
+    else:
+        while j <= i - 3:
+            print(" ", end="")
+            j += 1
+        j = 1
+        while j <= 2 * (6 - i) - 1:
+            print("*", end="")
+            j += 1
+    print()
+    i += 1
